d38f84d916ceabf1f7c633eecc35629ccd396e6c
Jayesh Tambe <tambe_jayesh@hotmail.com>
2021-09-18 22:56:05 -0700

    Update PhoneNumbersToCharsCombinations.cs

--- PhoneNumbersToCharsCombinations.cs
+++ PhoneNumbersToCharsCombinations.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace PhoneNumbersToChars
+namespace DFS_NonRecursive
 {
 
     //https://leetcode.com/problems/letter-combinations-of-a-phone-number/
@@ -11,20 +11,18 @@ namespace PhoneNumbersToChars
         0 <= digits.length <= 4
         digits[i] is a digit in the range ['2', '9'].
     */
-    
 
     public class PhoneNumbersToChars
     {
-
-
         public static IList<string> LetterCombinations(string digits)
         {
             IList<string> result = new List<string>();
+            // return if empty
             if (string.IsNullOrEmpty(digits))
             {
                 return result;
             }
-
+            // return if invalid input
             foreach (char c in digits)
             {
                 if ((int)c > (int)'9' || (int)c < (int)'2')
@@ -33,7 +31,7 @@ namespace PhoneNumbersToChars
                 }
 
             }
-
+            // set up dictionary 
             Dictionary<char, string> digitCharsMapping = new Dictionary<char, string>();
             digitCharsMapping.Add('2', "abc");
             digitCharsMapping.Add('3', "def");
@@ -44,7 +42,10 @@ namespace PhoneNumbersToChars
             digitCharsMapping.Add('8', "tuv");
             digitCharsMapping.Add('9', "wxyz");
 
+            // hashset is used to detect if a character is already visted
             HashSet<string> visited = new HashSet<string>();
+
+            // call stack is used to create string to add in result set
             Stack<char> callStack = new Stack<char>();
 
             for (int i = 0; i < digits.Length; i++)
@@ -53,6 +54,7 @@ namespace PhoneNumbersToChars
                 {
                     if (!visited.Contains(c+ "_"+ i))
                     {
+                        // hashset add c+_+i because, input can be "222" and we can determine which level of 2 is visited
                         visited.Add(c + "_" + i);
                         callStack.Push(c);
 
@@ -71,6 +73,7 @@ namespace PhoneNumbersToChars
                             // go back one step in digits
                             --i;
                         }
+                        // once entry is added, break to go back immediately
                         break;
                     }
 
@@ -97,8 +100,13 @@ namespace PhoneNumbersToChars
                         {
                             visited.Remove(s + "_" + i);
                         }
+                        // once you remove all nodes at a level from visited,
+                        // you need to go back to previous level.
+                        // here we are doing i = i-2 because the for loop will make it i++ and that will be i-1 for next iteration.
                         i = i - 2;
+                        // get the last element out from previous level, so that new branch of tree can be printed
                         callStack.Pop();
+                        // break foreach loop so that you can go back to outer for loop to start from i-1 level
                         break;
                     }
                 }
