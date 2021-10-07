bdf2ec656ffd493f8e5c392122c0915566c99951
Jayesh Tambe <tambe_jayesh@hotmail.com>
2021-10-06 21:53:06 -0700

    Create TrieDataStructure.cs

---
+++ TrieDataStructure.cs
@@ -0,0 +1,115 @@
+namespace ProblemSolving
+{
+    public class TrieNode
+    {
+        public char val;
+        public HashSet<char> childrenChars = new HashSet<char>();
+        public List<TrieNode> childrenNodes = new List<TrieNode>();
+        public bool isWord = false;
+    }
+    public class Trie
+    {
+        //https://leetcode.com/problems/implement-trie-prefix-tree/
+        // leetcode 208
+        
+        TrieNode root;
+        public Trie()
+        {
+            root = new TrieNode();
+            root.val = '*';
+        }
+
+        public void Insert(string word)
+        {
+            var currentTrieNode = root;
+            int charCounter = 0;
+            while(charCounter < word.Length)
+            {
+                while (currentTrieNode.childrenChars.Contains(word[charCounter]))
+                {
+                    currentTrieNode = currentTrieNode.childrenNodes.FirstOrDefault(x => x.val == word[charCounter]);
+                    if (charCounter == word.Length - 1)
+                    {
+                        currentTrieNode.isWord = true;
+                        break;
+                    }
+                    charCounter++;
+                }
+
+                var newTrieNode = new TrieNode();
+                newTrieNode.val = word[charCounter];
+                currentTrieNode.childrenNodes.Add(newTrieNode);
+                currentTrieNode.childrenChars.Add(word[charCounter]);
+                if (charCounter == word.Length - 1)
+                {
+                    newTrieNode.isWord = true;
+                    break;
+                }
+                else
+                {
+                    charCounter++;
+                    currentTrieNode = newTrieNode;
+                }
+            }
+
+        }
+
+        public bool Search(string word)
+        {
+            var currentTrieNode = root;
+            int charCounter = 0;
+            while (charCounter < word.Length && currentTrieNode.childrenChars.Contains(word[charCounter]))
+            {
+                currentTrieNode = currentTrieNode.childrenNodes.FirstOrDefault(x => x.val == word[charCounter]);
+                charCounter++;
+            }
+            charCounter--;
+            if (charCounter < word.Length -1)
+            {
+                return false;
+            }
+            return currentTrieNode.isWord;
+            
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            var currentTrieNode = root;
+            int charCounter = 0;
+            while (charCounter < prefix.Length && currentTrieNode.childrenChars.Contains(prefix[charCounter]))
+            {
+                currentTrieNode = currentTrieNode.childrenNodes.FirstOrDefault(x => x.val == prefix[charCounter]);
+                charCounter++;
+            }
+            charCounter--;
+            if (charCounter < prefix.Length - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prefix Tree,Trie data structure");
+
+            Console.WriteLine();
+            Trie obj = new Trie();
+            obj.Insert("apple");
+            bool param = false;
+            param = obj.Search("apple");
+            param = obj.Search("applee");
+            param = obj.Search("app");
+            param = obj.StartsWith("app");
+            obj.Insert("app");
+            param = obj.Search("app");
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
