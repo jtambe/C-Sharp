7462b9f4f4fc96e445c2a488162789c329b186c5
Jayesh Tambe <tambe_jayesh@hotmail.com>
2021-10-09 10:07:34 -0700

    Create TaskScheduler_Leetcode621.cs

---
+++ TaskScheduler_Leetcode621.cs
@@ -0,0 +1,187 @@
+
+/*
+* LeetCode 621
+ * Given a characters array tasks, representing the tasks a CPU needs to do, 
+where each letter represents a different task. Tasks could be done in any order. 
+Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+However, there is a non-negative integer n that represents the cooldown period between two *same tasks* (the same letter in the array), 
+that is that there must be at least n units of time between any two same tasks.
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+ 
+*Example 1:*
+
+*Input:* tasks = ["A","A","A","B","B","B"], n = 2
+*Output:* 8
+
+*Explanation:* 
+A -> B -> idle -> A -> B -> idle -> A -> B
+
+There is at least 2 units of time between any two same tasks.
+
+*Example 2:*
+
+*Input:* tasks = ["A","A","A","B","B","B"], n = 0
+*Output:* 6
+
+*Explanation:* On this case any permutation of size 6 would work since n = 0.
+["A","A","A","B","B","B"]
+["A","B","A","B","A","B"]
+["B","B","B","A","A","A"]
+...
+And so on.
+
+*Example 3:*
+
+*Input:* tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+*Output:* 16
+
+*Explanation:* 
+One possible solution is
+A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace DFS_NonRecursive
+{
+    public class myTask
+    {
+        public string name;
+        public int frequency;
+    }
+    public class AmazonTaskScheduler
+    {
+
+        public static int TaskScheduler(string[] tasks, int n)
+        {
+            if (n == 0 || tasks.Length == 0)
+            {
+                return tasks.Length;
+            }
+
+            int taskCompletedCount = 0;
+            List<string> tasksCovered = new List<string>();
+            Dictionary<string, int> charFrequency = new Dictionary<string, int>();
+            Dictionary<string, int> addTasksForIdle = new Dictionary<string, int>();
+            Dictionary<string, int> charFreAux = new Dictionary<string, int>();
+
+            foreach (var task in tasks)
+            {
+                if (charFrequency.TryGetValue(task, out var fre))
+                {
+                    charFrequency[task] = fre + 1;
+                }
+                else
+                {
+                    charFrequency.Add(task, 1);
+                }
+
+                addTasksForIdle[task] = -1;
+                charFreAux[task] = -1;
+            }
+
+            myTask removeTask = new myTask();
+
+            // if I can use c# PriorityQueue from C# 10.0 over here it would be helpful to reduce this sorting of O(n log n)
+            charFrequency = charFrequency.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value); 
+
+            var pair = charFrequency.FirstOrDefault(x => x.Value > 0);
+            removeTask.name = pair.Key;
+            removeTask.frequency = pair.Value;
+            removeTask.frequency--;
+            
+            charFrequency[removeTask.name] = -1;
+
+            tasksCovered.Add(removeTask.name);
+
+            charFreAux[removeTask.name] = removeTask.frequency;
+
+            addTasksForIdle[removeTask.name] = n-1;
+
+            TaskSchedulerUtil(tasks, tasksCovered, charFrequency, addTasksForIdle, charFreAux, ++taskCompletedCount, n);
+            return tasksCovered.Count();
+        }
+
+        public static void TaskSchedulerUtil(string[] tasks, List<string> tasksCovered, 
+            Dictionary<string, int> charFrequency, Dictionary<string, int> addTasksForIdle, Dictionary<string, int> charFreAux,
+            int taskCompletedCount, int n)
+        {
+
+            if (taskCompletedCount == tasks.Length)
+            {
+                return;
+            }
+
+            myTask removeTask = new myTask();
+            // if I can use c# PriorityQueue from C# 10.0 over here it would be helpful to reduce this sorting of O(n log n)
+            charFrequency = charFrequency.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            var pair = charFrequency.FirstOrDefault(x => x.Value > 0);
+            if (pair.Key == null)
+            {
+                tasksCovered.Add("idle");
+            }
+            else
+            {
+                removeTask.name = pair.Key;
+                removeTask.frequency = pair.Value;
+                removeTask.frequency--;
+
+                charFrequency[removeTask.name] = -1;
+
+                tasksCovered.Add(removeTask.name);
+
+                charFreAux[removeTask.name] = removeTask.frequency;
+
+                taskCompletedCount++;
+            }
+
+            foreach (var key in addTasksForIdle.Keys.ToList())
+            {
+                if (addTasksForIdle[key] == 0)
+                {
+                    charFrequency[key] = charFreAux[key];
+                }
+                addTasksForIdle[key] = addTasksForIdle[key] - 1;
+                
+            }
+            if (removeTask.name != null)
+            {
+                addTasksForIdle[removeTask.name] = n-1;
+            }
+
+            TaskSchedulerUtil(tasks, tasksCovered, charFrequency, addTasksForIdle, charFreAux, taskCompletedCount, n);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Task Scheduler");
+
+            Console.WriteLine();
+            string[] input = { "A", "A", "A", "B", "B", "B" };
+            var result = TaskScheduler(input, 2);
+            Console.WriteLine(result);
+
+            string[] input2 = { "A", "A", "A", "B", "B", "B" };
+            result = TaskScheduler(input2, 0);
+            Console.WriteLine(result);
+
+            string[] input3 = { "A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G" };
+            result = TaskScheduler(input3, 2);
+            Console.WriteLine(result);
+
+            string[] input4 = { "A", "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "E" };
+            // A B C A B C D A B C D E
+            result = TaskScheduler(input4, 2);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+    }
+}
